629d110e1aee52537cc3e64361c1e97b246d35a9
CaiXiongkai <Caixiongkai@gmail.com>
2017-01-14 17:20:49 +0800

    init flask webapp files

---
+++ hello.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, make_response
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time = datetime.utcnow())
+
+@app.route('/request')
+def requeste():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s.</p>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
